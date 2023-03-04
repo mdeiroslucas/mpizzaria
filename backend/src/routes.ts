e19afe09e883f56f4eaa57179ab397aee4a8321e
Lucas Medeiros <lucas-medeiros@live.com>
2023-03-04 13:34:46 -0300

    adding a new category service and controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,23 +3,19 @@ import {CreateUserController} from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAutheticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ok: true})
-})
+//User routers
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
-router.get('/teste2', (req: Request, res: Response) => {
-  return res.json(
-    'esta tudo: certo'
-  )
-})
+//Category routers
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export {router}; 
