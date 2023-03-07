89dad1a85c9810e4a437616adad5c19912af0a3e
Lucas Medeiros <lucas-medeiros@live.com>
2023-03-07 05:38:33 -0300

    adding index configs

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { Inter } from 'next/font/google'
 import styles from '../../styles/Home.module.scss';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Lucas Medeiros</h1>
-    </div>
+    <>
+      <Head>
+        <title>Mpizzaria - Faça seu Login</title>
+      </Head>
+      <div>
+          <h1>MPizzaria</h1>
+      </div>
+    </>
   )
 }
