3bfddec4270e5b7e38b321b99ba4ad566eaefc03
Lucas Medeiros <lucas-medeiros@live.com>
2023-03-09 06:42:26 -0300

    adding page already user and signup router

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,27 +1,32 @@
 import { Inter } from 'next/font/google'
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 import Head from 'next/head';
 import Image from 'next/image';
 import logoImg from '../../public/mpizzaria.png';
 
 import Link from 'next/link';
 
-import {Input} from '../components/ui/input';
-import {Button} from '../components/ui/button';
+import {Input} from '../../components/ui/input';
+import {Button} from '../../components/ui/button';
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Mpizza - Faça seu Login</title>
+        <title>Faça seu cadastro agora</title>
       </Head>
       <div className={styles.containerCenter}>
           {/* <Image src={logoImg} alt="Logo MPizzaria"/> */}
 
 
       <div className={styles.login}>
+        <h1>Criando sua conta</h1>
         <form action="">
+          <Input
+            placeholder='Digite o seu nome'
+            type='text'        
+          />
           <Input
             placeholder='Digite o seu email'
             type='text'        
@@ -34,12 +39,12 @@ export default function Home() {
           <Button
           type="submit"
           loading={false}>
-            Acessar
+            Cadastrar
           </Button>
         </form>
 
-        <Link href="/signup" className={styles.text}>
-          Nao possui uma conta? Cadastre-se!
+        <Link href='/' className={styles.text}>
+          Já possui uma conta? Faça o login!
         </Link>
 
         </div>
